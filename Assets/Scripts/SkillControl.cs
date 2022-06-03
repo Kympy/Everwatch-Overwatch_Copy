9abd1e9cd697af8f6e608d769124b1859f11aae6
Kympy <gftr9905@gmail.com>
2022-06-03 14:43:53 +0900

    트레이서 시간역행 구현 (Ver 0.1.5)
    1. 3초 동안 경로를 따라 포지션을 저장하고 시간역행 코루틴을 호출하여 배열에 저장된 역순으로 1.5초동안 복귀함.
    
    2. 스택이나 큐를 쓰려고 했지만 위치 저장 시에는 스택, 꺼내 쓸 때는 큐가 필요해서 배열을 쓰는 것이 더 적합하다고 판단했음.

--- Assets/Scripts/SkillControl.cs
+++ Assets/Scripts/SkillControl.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class SkillControl : MonoBehaviour
 {
-    private Rigidbody m_rigidbody;
+    //private Rigidbody m_rigidbody;
     public Transform Camera;
     public float power;
     public Vector3 desiredDir;
     public Vector3 destination;
-
+    public ArrayList originalPosition;
+    
     public float speed;
     public float range;
     public float num;
     public float cameraHeight;
-
+    private float timer;
+    
     private bool isBlink;
+    private bool isSave;
     void Start()
     {
-        m_rigidbody = GetComponent<Rigidbody>();
-        isBlink = false;
-    }
-    void OnKeyBoard()
-    {
-        
-
+        //m_rigidbody = GetComponent<Rigidbody>();
+        isBlink = false; isSave = true; timer = 0f;
+        originalPosition = new ArrayList();
     }
     void Update()
     {
+        SavePosition(isSave);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(TimeTravel());
+        }
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             Blink();
@@ -42,17 +47,53 @@ public class SkillControl : MonoBehaviour
             else isBlink = false;
         }
     }
+    void SavePosition(bool start)
+    {
+        if (start)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 0.1f)
+            {
+                if (originalPosition.Count < 30) // 3초 전까지 저장
+                {
+                    originalPosition.Add(transform.position);
+                    //Debug.Log(originalPosition.Count);
+                  
+                }
+                else
+                {
+                    originalPosition.RemoveAt(0); // 30이상으로 저장되면 첫번째 요소부터 지운다. 자동으로 인덱스 당겨짐
+                    originalPosition.Add(transform.position);
+                    //Debug.Log(originalPosition.Count);
+                }
+                timer = 0f;
+            }
+        }
+        else return;
+    }
+    IEnumerator TimeTravel()
+    {
+        isSave = false;
+        for(int i = originalPosition.Count - 1; i >= 0; i--) // 29~ 0까지 30개
+        {
+            transform.position = Vector3.MoveTowards(transform.position, (Vector3)originalPosition[i], Time.deltaTime * speed * 2);          
+            //Debug.Log(originalPosition[i]);
+            yield return new WaitForSeconds(0.02f); // 총 1.5 초가 걸려서 돌아온다.
+        }
+        isSave = true;
+    }
     void Blink()
     {
         RaycastHit hit;
-
         if (Physics.Raycast(Camera.position, Camera.forward, out hit, range))
         {
-            Debug.DrawLine(Camera.position, hit.point * num, Color.red,2);
+            //Debug.DrawLine(Camera.position, hit.point * num, Color.red,2);
             // 장애물이 있으면 
-            Debug.Log("Hit");
-            destination = hit.point * num; // 목적지보다 조금 덜 간다
-
+            if(hit.transform.tag != "Bot")
+            {
+                Debug.Log("Hit");
+                destination = hit.point * num; // 목적지보다 조금 덜 간다
+            }
         }
         else // 장애물이 없으면
         {
